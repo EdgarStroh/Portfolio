321dbe65ec24ea0335c59577456b7ea8043e86f1
EdgarStroh <edgarstroh@googlemail.com>
2025-03-12 14:35:26 +0100

    started portfolio project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HeroComponent } from './hero/hero.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component'
+import { SkillsComponent } from './skills/skills.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    // RouterOutlet, 
+    HeaderComponent,
+    AboutMeComponent, HeroComponent, 
+    ProjectsComponent, ContactComponent, 
+    SkillsComponent, TestimonialsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HeroComponent }, // Startseite (alles sichtbar)
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent }
+];
