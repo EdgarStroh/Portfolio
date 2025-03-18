bcb4bbb9f0edfb63700586c7fa1c7329d0039ed1
EdgarStroh <edgarstroh@googlemail.com>
2025-03-18 16:34:50 +0100

    added hover effects on hero section

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [  HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+  hasHovered = false;
+  isReversing = false;
 
+  onMouseEnter() {
+    this.hasHovered = true;
+    this.isReversing = false; // Verhindert, dass direkt die Reverse-Animation startet
+  }
+
+  onMouseLeave() {
+    if (this.hasHovered) {
+      this.isReversing = true; // Setzt die Reverse-Animation aktiv
+    }
+  }
 }
