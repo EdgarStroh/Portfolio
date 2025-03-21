ac0b521f22ea9fc25afe300a49831667ca7aac26
EdgarStroh <edgarstroh@googlemail.com>
2025-03-21 15:38:24 +0100

    skills component done

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-skills',
   standalone: true,
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
+  imports: [CommonModule]
 })
 export class SkillsComponent {
+  stickerImage: string = '/assets/img/skills/sticker.png';
+  private isOpen: boolean = false;
 
+  onStickerClick(): void {
+    if (!this.isOpen) {
+      this.stickerImage = '/assets/img/skills/sticker2.png';
+      setTimeout(() => {
+        this.stickerImage = '/assets/img/skills/sticker-open.png';
+        this.isOpen = true;
+      }, 10);
+    } else {
+      this.stickerImage = '/assets/img/skills/sticker.png';
+      this.isOpen = false;
+    }
+  }
 }
