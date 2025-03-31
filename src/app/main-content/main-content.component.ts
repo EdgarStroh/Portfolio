72a6d7b54768a631f35ac6cc8ce007470931b791
EdgarStroh <edgarstroh@googlemail.com>
2025-03-31 13:49:29 +0200

    small changes on contact

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { HeroComponent } from './hero/hero.component';
 import { ProjectsComponent } from './projects/projects.component';
-// import { ContactComponent } from './contact/contact.component';
+import { ContactComponent } from './contact/contact.component';
 import { SkillsComponent } from './skills/skills.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 
@@ -15,7 +15,7 @@ import { TestimonialsComponent } from './testimonials/testimonials.component';
     AboutMeComponent,
     HeroComponent,
     ProjectsComponent,
-    // ContactComponent,
+    ContactComponent,
     SkillsComponent,
     TestimonialsComponent
   ],
