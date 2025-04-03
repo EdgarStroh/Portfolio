c21a147bc0f98a5086e871401b239a6002aee887
EdgarStroh <edgarstroh@googlemail.com>
2025-04-03 21:15:49 +0200

    small changes

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  hasHovered: boolean[] = [false, false, false]; 
 
+  onMouseEnter(index: number) {
+    this.hasHovered[index] = true;
+  }
+
+  onMouseLeave(index: number) {
+    this.hasHovered[index] = false;
+  }
+
+
+  // hasHovered = false;
+  // isReversing = false;
+
+  // onMouseEnter() {
+  //   this.hasHovered = true;
+  //   this.isReversing = false;
+  // }
+
+  // onMouseLeave() {
+  //   if (this.hasHovered) {
+  //     this.isReversing = true;
+  //   }
+  // }
 }
