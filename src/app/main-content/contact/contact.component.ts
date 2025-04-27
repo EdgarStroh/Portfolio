ffe30f6cc9f97dbf6997fb9dae5d52e7ff24d18f
EdgarStroh <edgarstroh@googlemail.com>
2025-04-27 17:51:05 +0200

    made contact form

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../shared/footer/footer.component";
+import { ContactformComponent } from "../../contactform/contactform.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
-  imports: [FooterComponent]
+  imports: [FooterComponent, ContactformComponent]
 })
 export class ContactComponent {
 
